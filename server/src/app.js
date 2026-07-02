d7fd66b5f040c0e5df480200366bbcd277a035e0
ajayrajput11 <ajayrajput171204@gmail.com>
2026-07-02 16:20:59 +0530

    fix cors

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
